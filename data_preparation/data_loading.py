222db21a051599a0fe354e230fc819b9b769f8d0
Tommaso Crippa <crippa.tommaso@gmail.com>
2026-03-24 10:37:43 +0000

    extend DataLoading with MTS support

--- data_preparation/data_loading.py
+++ data_preparation/data_loading.py
@@ -1,5 +1,6 @@
 """
 Copyright 2019 Marco Lattuada
+Copyright 2026 Tommaso Crippa
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -72,16 +73,31 @@ class DataLoading(data_preparation.data_preparation.DataPreparation):
             self._logger.error("input_path must be a path string to a dataset or a pandas.DataFrame")
             sys.exit(1)
 
+        # Sort by time_column and drop it (activates time series adapter)
+        time_col = self._campaign_configuration['DataPreparation'].get('time_column', None)
+        series_id_col = self._campaign_configuration['DataPreparation'].get('series_id_column', None)
+        if time_col:
+            if time_col not in data_frame.columns:
+                self._logger.error("time_column '%s' not found in dataset", time_col)
+                sys.exit(1)
+            data_frame[time_col] = pd.to_datetime(data_frame[time_col])
+            data_frame = data_frame.sort_values(by=time_col).reset_index(drop=True)
+            data_frame = data_frame.drop(columns=[time_col])
+
+        y_col = self._campaign_configuration['General']['y']
         self._campaign_configuration['Features'] = {}
         self._campaign_configuration['Features']['Original_feature_names'] = []
         for column_name in data_frame.columns.values:
-            if column_name != self._campaign_configuration['General']['y']:
-                self._campaign_configuration['Features']['Original_feature_names'].append(column_name)
+            # Exclude y column and series_id_column from feature names
+            if column_name == y_col:
+                continue
+            if series_id_col and column_name == series_id_col:
+                continue
+            self._campaign_configuration['Features']['Original_feature_names'].append(column_name)
 
         inputs_split = {}
         inputs_split["training"] = data_frame.index.values.tolist()
         inputs_split["all"] = inputs_split["training"].copy()
 
-        output = regression_inputs.RegressionInputs(data_frame, inputs_split, self._campaign_configuration['Features']['Original_feature_names'],
-                                                                              self._campaign_configuration['General']['y'])
+        output = regression_inputs.RegressionInputs(data_frame, inputs_split, self._campaign_configuration['Features']['Original_feature_names'], y_col)
         return output
